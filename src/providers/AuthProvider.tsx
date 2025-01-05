0e61c3444174de6fea2a7a0cad0e572c9238eae8
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2025-01-05 17:27:44 +0800

    add auth provider

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<string | null>(localStorage.getItem('accessToken'));
+
+  const login = (token: string) => {
+    localStorage.setItem('accessToken', token);
+    setUser(token);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('accessToken');
+    setUser(null);
+  };
+
+  const isAuthenticated = Boolean(user);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
