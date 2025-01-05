2add506611e9c0a8434c8eee7959312cf3214f41
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2025-01-05 17:35:26 +0800

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import ProtectedRoute from './pages/ProtectedRoute';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 const App = () => (
   <Routes>
