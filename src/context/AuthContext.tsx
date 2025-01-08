1cd96eb591dab2b2f4234f7f50d43497a40c9378
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2025-01-08 17:19:18 +0800

    delete file

--- src/context/AuthContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-import { AuthContextType } from '../types/context/AuthContextType';
-
-export const AuthContext = createContext<AuthContextType | undefined>(undefined);
