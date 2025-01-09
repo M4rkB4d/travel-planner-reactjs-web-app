8a423faff2d12d127f9cd6e9236062440ccf2f85
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2025-01-09 11:23:28 +0800

    update login form by implementing reusable button component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-import { loginApi } from '../api/AuthApi';
-import { useAuthHook } from '../hooks/AuthHook';
-import { useNavigate } from 'react-router-dom';
-import { InputTextField } from './common/Input';
+import React, { useState } from "react";
+import { loginApi } from "../api/AuthApi";
+import { useAuthHook } from "../hooks/AuthHook";
+import { useNavigate } from "react-router-dom";
+import { InputTextField } from "./common/Input";
+import { Button } from "./common/Button";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { login } = useAuthHook();
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const data = await loginApi({ email, password});  // Get login data
-      login(data.accessToken);  // Store token in context
-      navigate('/');  // Redirect to home
+      const data = await loginApi({ email, password }); // Get login data
+      login(data.accessToken); // Store token in context
+      navigate("/"); // Redirect to home
     } catch (err) {
       console.log(err);
-      setError('Invalid email or password');
+      setError("Invalid email or password");
     }
   };
 
   return (
-    <form 
-    onSubmit={handleSubmit}
-    className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md"
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md"
     >
       <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
       <InputTextField
@@ -37,7 +38,7 @@ const LoginForm = () => {
         required
         variant="filled"
         label="Email"
-        errorMessage={error ? 'Invalid email' : ''}
+        errorMessage={error ? "Invalid email" : ""}
       />
       <InputTextField
         type="password"
@@ -47,14 +48,11 @@ const LoginForm = () => {
         required
         variant="filled"
         label="Password"
-        errorMessage={error ? 'Invalid password' : ''}
+        errorMessage={error ? "Invalid password" : ""}
       />
-      <button
-        type="submit"
-        className="w-full mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
-      >
+      <Button variant="primary" onClick={handleSubmit}>
         Login
-      </button>
+      </Button>
     </form>
   );
 };
