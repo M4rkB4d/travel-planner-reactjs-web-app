66a1955fb0f0930858465916e2f98494a6bb7c83
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2025-01-09 18:44:53 +0800

    update ui

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -27,35 +27,51 @@ const LoginForm = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="min-w-full max-w-8 bg-transparent"
+      className="bg-transparent max-w-sm mx-auto space-y-10"
     >
-      <h1 className="text-4xl font-bold text-center">Welcome</h1>
-      <h2 className="text-sm font-bold text-center">Login with Email</h2>
-      <div>
-      <InputTextField
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        variant="underlined"
-        label="Email"
-        errorMessage={error ? "Invalid email" : ""}
-      />
+      {/* Heading Section */}
+      <div className="space-y-2">
+        <h1 className="text-4xl font-bold text-center">Welcome</h1>
+        <h2 className="text-sm font-bold text-center">Login with Email</h2>
       </div>
-      <InputTextField
-        type="password"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        variant="underlined"
-        label="Password"
-        errorMessage={error ? "Invalid password" : ""}
-      />
-      <Button variant="primary" onClick={handleSubmit}>
-        Login
-      </Button>
+
+      {/* Input Fields */}
+      <div className="space-y-0 w-full md:w-96">
+        {/* Email Input */}
+        <InputTextField
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          variant="underlined"
+          label="Email"
+          errorMessage={error ? "Invalid email" : ""}
+        />
+
+        {/* Password Input */}
+        <InputTextField
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          variant="underlined"
+          label="Password"
+          errorMessage={error ? "Invalid password" : ""}
+        />
+      </div>
+
+      {/* Submit Button */}
+      <div className="text-center">
+        <Button
+          variant="primary"
+          onClick={handleSubmit}
+          className="w-full md:w-40"
+        >
+          Login
+        </Button>
+      </div>
     </form>
   );
 };
